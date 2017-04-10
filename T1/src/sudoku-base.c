806d7a0e48a50e507ed3ebc19813ec8d15a2ec98
Montezano <pablo.montezano@gmail.com>
2017-04-10 20:36:09 +0000

    Dividida as tarefas em métodos.

--- T1/src/sudoku-base.c
+++ T1/src/sudoku-base.c
@@ -22,42 +22,56 @@ int load_grid(int grid[][SIZE], char *filename) {
 /**
 *	It looks for sudoku errors
 **/
-void verify(int grid[][SIZE]) {
-	int mult = 1;
-	int mult_column = 1;
-	int columns[SIZE] = {[0 ... (SIZE-1)] = 1}; // it works with gcc
-	int region[SIZE] = {[0 ... (SIZE-1)] = 1};
+void verify_line(int grid[][SIZE]) {
+	int mult_line = 1;
 
 	for(int i = 0; i < 81; i++) {
-		int result_line = grid[i/SIZE][i%SIZE]; // grid[x][y] x = position/9, y = position%9 ... position [0 .. 80] //*(grid[0]+i)//
-		int result_column = grid[i%SIZE][i/SIZE];
-
-		// columns[i%SIZE] *= result_line;
-		region[((i%SIZE)/3)+(((i/SIZE)/3)*3)] *= result_line; // Add it to a region
-
-		mult *= result_line;
-		mult_column *= result_column;
+		mult_line *= grid[i/SIZE][i%SIZE]; // grid[x][y] x = position/9, y = position%9 ... position [0 .. 80] //*(grid[0]+i)//
 
 		if((i+1)%SIZE == 0){
-			if( mult != MAX)
+			if( mult_line != MAX)
 				printf("erro na linha %d\n", (i-1)/SIZE);
-			if(mult_column != MAX)
-				printf("erro na coluna %d\n", (i-1)/SIZE);
-			mult = 1;
-			mult_column = 1;
-
+			mult_line = 1;
 		}
 	}
-
-	for(int i = 0; i < SIZE; i++){
-		// if(columns[i] != MAX)
-		// 	printf("erro na coluna %d\n", i);
-		if(region[i] != MAX)
-			printf("erro na regiao %d\n", i);
-	}
-
 }
 
+void verify_column(int grid[][SIZE]) {
+	int mult_column = 1;
+
+	for(int i = 0; i < 81; i++) {
+		mult_column *= grid[i%SIZE][i/SIZE]; // grid[x][y] x = position/9, y = position%9 ... position [0 .. 80] //*(grid[0]+i)//		
+
+		if((i+1)%SIZE == 0){
+			if( mult_column != MAX)
+				printf("erro na coluna %d\n", (i-1)/SIZE);
+			mult_column = 1;
+		}
+	}
+}
+
+void verify_region(int grid[][SIZE]) {
+	int mult_region = 1;
+	
+	//int region[SIZE] = {[0 ... (SIZE-1)] = 1};
+
+	for(int i = 0; i < 81; i++) {
+		mult_region *= grid[(((i/SIZE)/3)*3)][((i%SIZE)/3)]; // grid[x][y] x = position/9, y = position%9 ... position [0 .. 80] //*(grid[0]+i)//
+		mult_region *= grid[(((i/SIZE)/3)*3)][((i%SIZE)/3)];
+		mult_region *= grid[(((i/SIZE)/3)*3)][((i%SIZE)/3)];
+
+	//	int result_column = grid[i%SIZE][i/SIZE];
+printf("%d %d %d\n", grid[(((i/SIZE)/3)*3)][((i%SIZE)/3)],
+	grid[((((i+1)/SIZE)/3)*3)][(((i+1)%SIZE)/3)],
+	grid[((((i+2)/SIZE)/3)*3)][(((i+2)%SIZE)/3)]);
+		// columns[i%SIZE] *= result_region;
+		if((i+1)%SIZE == 0){
+			if( mult_region != MAX)
+				printf("erro na regiao %d\n", (i-1)/SIZE);
+			mult_region = 1;
+		}
+	}	
+}
 
 int main(int argc, char *argv[]) {
 
@@ -76,7 +90,9 @@ int main(int argc, char *argv[]) {
 			printf("\n");
 		}
 		printf("\n");
-		verify(grid);
+		verify_line(grid);
+		verify_column(grid);
+		verify_region(grid);
 	}
 
 	return 0;
